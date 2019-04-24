b7b1b517e5ae8d99f6a8e1cba6dd7594ab2a88e6
oleks <olekstaruch@gmail.com>
2019-04-24 18:53:34 +0200

    update

---
+++ KolokwiumNr1/DAL/DB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolokwiumNr1.DAL
+{
+    public class DB
+    {
+        PjatkDb pjatkDb;
+        public DB()
+        {
+            pjatkDb = new PjatkDb();
+        }
+
+        public List<Emp> GetEmps()
+        {
+            var emp = pjatkDb.Students;
+
+            return emp.Include(s => s.Study).ToList();
+        }
+
+        public List<Dept> GetDept()
+        {
+            var dept = pjatkDb.Students;
+
+            return dept.ToList();
+        }
+
+
+        public void AddEmp(Emp emp)
+        {
+            pjatkDb.Students.Add(emp);
+
+            pjatkDb.SaveChanges();
+        }
+
+
+    }
+}
--- KolokwiumNr1/MainWindow.xaml.cs
+++ KolokwiumNr1/MainWindow.xaml.cs
@@ -20,9 +20,98 @@ namespace KolokwiumNr1
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Emp> list;
         public MainWindow()
         {
             InitializeComponent();
+            SetDataGrid();
+            SetComboBox();
         }
-    }
+
+        public void SetDataGrid()
+        {
+            //DataGrid.Items.Clear();
+            var l = new DB().GetEmps();
+            var resoult = from emp in l
+                          select emp;
+            list = new ObservableCollection<Emp>(resoult.ToList());
+
+            DataGrid.ItemsSource = list;
+        }
+
+        public void SetComboBox()
+        {
+            var list = new DB().GetDepts();
+            ComboBox.Items.Clear();
+            foreach (Dept s in list)
+            {
+                ComboBox.Items.Add(s.Name);
+            }
+            ComboBox.SelectedItem = list[0].Name;
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            var resoult = from emp in new DB().GetEmps()
+                          where emp.Ename == Text3.Text
+                          select emp;
+
+            DataGrid.ItemsSource = resoult;
+        }
+
+        private void ShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            DataGrid.ItemsSource = new DB().GetEmps();
+        }
+
+        public bool Check()
+        {
+            int i = 0;
+            if (String.IsNullOrWhiteSpace(Text1.Text))
+            {
+                i++;
+                Text1.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                Text1.BorderBrush = new SolidColorBrush(Colors.Black);
+            }
+            if (String.IsNullOrWhiteSpace(Text1.Text))
+            {
+                i++;
+                Text1.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                Text1.BorderBrush = new SolidColorBrush(Colors.Black);
+            }
+            if (i == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (Check())
+            {
+
+
+
+
+
+
+                Text1.Text = "";
+                Text2.Text = "";
+                //ComboBox
+
+            }
+            
+        }
+
 }
